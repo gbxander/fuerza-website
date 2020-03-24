6e3641e9c03dd6c1300cac1bd290a5aab211c4ca
gbxander <giovanni.bonilla@9dots.org>
2020-03-23 21:47:56 -0700

    create a thank you page

---
+++ src/pages/thanks.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Thanks from "../components/Thanks/Thanks"
+import LayoutWrapper from "../components/LayoutWrapper/LayoutWrapper"
+
+const ThankssGUI = () => {
+  return (
+    <LayoutWrapper tabKey={"5"} header={false}>
+      <Thanks />
+    </LayoutWrapper>
+  )
+}
+
+export default ThankssGUI
